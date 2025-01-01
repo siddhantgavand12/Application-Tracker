72823ac414e4b9a13bae3efbe35c4860c40d55e2
Siddhant Gavand <siddhantgavand74@gmail.com>
2025-01-02 02:57:25 +0530

    Deployment changes1

--- backend/server.js
+++ backend/server.js
@@ -9,7 +9,13 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 // Middleware
-app.use(cors());
+app.use(cors(
+    {
+        origin: ["https://application-tracker-seven.vercel.app"],
+        methods:["POST","GET","PATCH"],
+credentials: true
+    }
+));
 app.use(express.json()); // Parse JSON bodies
 
 // MongoDB connection
