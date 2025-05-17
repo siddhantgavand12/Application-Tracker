06125b9ece6a6d526f65c08715220cfef436546d
Siddhant Gavand <siddhantgavand74@gmail.com>
2025-05-17 18:43:30 +0530

    Some Changes in footer

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -3,7 +3,7 @@ const Footer = () => {
 
     return (
         <footer className="bg-indigo-600 text-white p-4 shadow-md w-full mt-auto">
-        <p className="text-center text-sm">&copy; {new Date().getFullYear()} MMC PTV Ltd. All rights reserved.</p>
+        <p className="text-center text-sm">&copy; {new Date().getFullYear()} MMC Pvt Ltd. All rights reserved.</p>
       </footer>
     );
 };
